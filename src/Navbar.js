4e4579b80171854c6c4704435508623a8c33988f
Vinod Reddy <vinodreddy52@gmail.com>
2025-03-13 22:07:58 +0530

    tech hub class added

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => (
   <AppBar position="fixed" color="primary">
     <Toolbar>
-      <Typography variant="h6" sx={{ flexGrow: 1 }}>Vinod TechHub</Typography>
+      <Typography variant="h6" component={Link} to="/" className="tech_hub" sx={{ flexGrow: 1 }}>Vinod TechHub</Typography>
       <Button color="inherit" component={Link} to="/">Home</Button>
       <Button color="inherit" component={Link} to="/about">About</Button>
       <Button color="inherit" component={Link} to="/skills">Skills</Button>
